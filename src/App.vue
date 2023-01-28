001becbd659e2cf02e224eb88c1c9c369610b05a
Nicolas <nicolasgabriel.developer@gmail.com>
2023-01-28 07:59:51 -0300

    Arquivos iniciais

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>ola</h1>
+
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+
+});
+</script>
+
+<style>
+
+</style>
